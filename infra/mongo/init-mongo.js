9d016cc543d820f04c2774b7f10303b1954b14ea
Leandrots82 <Leandrots82@gmail.com>
2026-06-10 18:59:59 -0300

    chore(infra): add rabbitmq service and update mongo init script

--- infra/mongo/init-mongo.js
+++ infra/mongo/init-mongo.js
@@ -3,7 +3,7 @@ db = db.getSiblingDB('taskflow');
 db.createCollection('jobs');
 db.createCollection('outbox_messages');
 
-db.jobs.createIndex({ idempotencyKey: 1 }, { unique: true });
+db.jobs.createIndex({ idempotencyKey: 1 }, { unique: true, sparse: true });
 db.jobs.createIndex({ status: 1, priority: -1, createdAt: 1 });
 db.jobs.createIndex({ scheduledAt: 1, status: 1 });
 db.outbox_messages.createIndex({ published: 1, createdAt: 1 });
